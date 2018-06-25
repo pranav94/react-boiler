c30bd47c2bd58fb9230e17b771212d369a059e91
Pranav Udupa <pranav.udupa@healthify.co>
2018-06-25 12:22:56 +0530

    Add boiler plate code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>
+        React boilerplate.
+      </h1>
+    )
+  }
+}
+
+export default App;
